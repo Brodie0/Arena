27db6702fd0b78ecd91aea0aebe9b2cb8b36f21c
Brodie0 <michal-szreder55@wp.pl>
2018-12-17 23:52:36 +0100

    Made lich spawnable, needs better movement and sounds

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager
+{
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        var startPosition =  new Vector3(260, 1.2f, 160);
+        GameObject player = Instantiate(playerPrefab, startPosition, Quaternion.identity);
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+    }
+}
--- Assets/Scripts/OnHitBySpellScript.cs
+++ Assets/Scripts/OnHitBySpellScript.cs
@@ -2,8 +2,8 @@
 
 namespace Assets.Scripts {
     public class OnHitBySpellScript : MonoBehaviour {
-        private AudioSource _src;
-        private AudioClip _explosion;
+        public AudioSource _src;
+        public AudioClip _explosion;
         void OnParticleCollision(GameObject other){
             _src.PlayOneShot(_explosion, 1f);
         }
