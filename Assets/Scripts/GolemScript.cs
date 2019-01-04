63ebbdb33dd37f1bf08ee50ba541bb894b92a6e5
Brodie0 <michal-szreder55@wp.pl>
2019-01-04 21:24:28 +0100

    Added golem

---
+++ Assets/Scripts/GolemScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts;
+using UnityEngine;
+
+public class GolemScript : PlayerScript {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
