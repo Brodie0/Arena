781a3c2503c107f7defcc341993bc901e2fa830f
Brodie0 <michal-szreder55@wp.pl>
2019-01-04 21:05:33 +0100

    Added grunt

---
+++ Assets/Scripts/GruntScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts;
+using UnityEngine;
+
+public class GruntScript : PlayerScript {
+
+    public AudioClip whack;
+
+    void Start () {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        MaxHp = 20;
+        Hp = MaxHp;
+        base.Start();
+    }
+	
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        if (!IsDead) {
+            if (Input.GetKeyDown (KeyCode.Q)) {
+                Anim.SetTrigger (Random.Range (0, 2) == 0 ? attack01Hash : attack02Hash);
+                AudioSource.PlayOneShot (whack, 1f);
+            }
+        }
+    }
+}
