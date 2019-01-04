a0fd5b4bd40ad68c38413c3fac49c289591246ff
Brodie0 <michal-szreder55@wp.pl>
2019-01-04 22:48:05 +0100

    Fixed new characters animations

--- Assets/Scripts/GolemScript.cs
+++ Assets/Scripts/GolemScript.cs
@@ -5,13 +5,29 @@ using UnityEngine;
 
 public class GolemScript : PlayerScript {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public AudioClip whack;
+
+    void Start () {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        MaxHp = 20;
+        Hp = MaxHp;
+        base.Start();
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        if (!IsDead) {
+            if (Input.GetKeyDown (KeyCode.Q)) {
+                Anim.SetTrigger (Random.Range (0, 2) == 0 ? attack01Hash : attack02Hash);
+                AudioSource.PlayOneShot (whack, 1f);
+            }
+        }
+    }
 }
